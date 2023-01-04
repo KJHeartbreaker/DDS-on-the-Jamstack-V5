9c020760fff55282662e54a757f220276a59ed31
KJHeartbreaker <kay.jay.obrien@gmail.com>
2023-01-03 22:17:37 -0700

    Added placeholder for grid

--- web/src/components/Grids/EquipmentGrid.tsx
+++ web/src/components/Grids/EquipmentGrid.tsx
@@ -2,6 +2,7 @@
 import { Link } from "gatsby";
 import React from "react";
 import Figure from "../Figure/Figure";
+import { StaticImage } from "gatsby-plugin-image"
 import { EquipmentGridCard, EquipmentGridContainer } from "./Grid.styles";
 
 export default function EquipmentGrid({ title, content }) {
@@ -14,11 +15,15 @@ export default function EquipmentGrid({ title, content }) {
             <Link to={`/equipment/${el.equipment.slug.current}`}>
               <h3>{el.equipment.title}</h3>
               ...Read More
-              <Figure
-                asset={el.equipment.images[0].asset}
-                alt={el.equipment.images[0].alt}
-                {...el.equipment.images[0]}
-              />
+              {el.equipment.images ? (
+                <Figure
+                  asset={el.equipment.images[0].asset}
+                  alt={el.equipment.images[0].alt}
+                  {...el.equipment.images[0]}
+                />
+              ) : (
+                <StaticImage src="https://cdn.sanity.io/images/je1gp4wx/production/3ee840906f6c5d1c66b4ff9a2f2f015b9a0f6a6e-362x294.svg" alt="Contact for images" />
+              )}
             </Link>
           </EquipmentGridCard>
         ))}
